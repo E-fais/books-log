7edc41b1aeeafdad1ded1dcf7bcf3021e04b3e28
fais edathil <faiesedathil@gmail.com>
2024-07-09 17:00:33 +0530

    bug fix

--- app.py
+++ app.py
@@ -1,43 +1,42 @@
 import pandas as pd
 
-books={'Name':[],'Author':[],'Type':[],'Notes':[]}
+books = {'Name': [], 'Author': [], 'Type': [], 'Notes': []}
 
 def start_app():
     while True:
-         print('BOOK OF BOOKS\n----------')
-         choice=input('Reply with 1 or 2 \n 1.Get books data\n 2.Add new book')
-         if choice=='1':
+        print('BOOK OF BOOKS\n----------')
+        choice = input('Reply with 1 or 2 \n 1.View books \n 2.Add new book:\n')
+        if choice == '1':
             try:
-               books= pd.read_csv('books.csv',sep='\t')
-               print(books)
+                books_df = pd.read_csv('books.csv', sep='\t')  # Debug: Changed 'books' to 'books_df'
+                print(books_df)
             except FileNotFoundError:
-                  print('No books found!')           
+                print('No books found!')           
 
-         elif choice=='2':
-            name=input('name of the book : ')
-            author=input('name of the author : ')
-            type=input('type : ')
-            notes=input('notes : ')
-            add_book(name,author,type,notes)
-            books_data=pd.DataFrame(books)
-            # print(books_data)
-            books_data.to_csv('books.csv',sep='\t',index=False)
+        elif choice == '2':
+            name = input('name of the book : ')
+            author = input('name of the author : ')
+            book_type = input('type : ')  # Debug: Changed 'type' to 'book_type' to avoid conflict with built-in type
+            notes = input('notes : ')
+            add_book(name, author, book_type, notes)  # Debug: Changed 'type' to 'book_type'
+            books_data = pd.DataFrame(books)
+            books_data.to_csv('books.csv', sep='\t', index=False)
+            print('book saved successfully!')
             
-         else:
-              print('\n enter a valid input \n')
-         exit=input('continue program..? (y/n)')
-         if exit.lower()=='y':
-                pass
+        else:
+            print('\n enter a valid input \n')
+        
+        exit = input('continue program..? (y/n)')
+        if exit.lower() == 'y':
+            pass
+        else:
+            print('program terminated')  # Debug: Added 'print' to show termination message
+            break
 
-         else:
-               ('program terminated')
-               break
-       
-    
-def add_book(name,author,type,notes):
+def add_book(name, author, book_type, notes):  # Debug: Changed 'type' to 'book_type'
     books['Name'].append(name)
     books['Author'].append(author)
-    books['Type'].append(type)
+    books['Type'].append(book_type)  # Debug: Changed 'type' to 'book_type'
     books['Notes'].append(notes)
 
 start_app()
